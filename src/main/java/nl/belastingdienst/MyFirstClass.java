8c00146465e26b0799e31e0da22903db0c000c74
bramjanssens <bram.janssens@infosupport.com>
2021-09-08 10:34:19 +0200

    My first commit

---
+++ src/main/java/nl/belastingdienst/MyFirstClass.java
@@ -0,0 +1,14 @@
+package nl.belastingdienst; // package name
+
+public class MyFirstClass { // class name
+
+    // Fully qualified class name:
+    // nl.belastingdienst.MyFirstClass
+
+    // commentaar  = single line comment
+
+    public static void main(String[] args) { // de main methode
+        System.out.println("String hello world!");
+    }
+
+}
