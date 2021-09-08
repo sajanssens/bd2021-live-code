218b22f9cdfe4123ccf6d063ab645bce3cd6400a
bramjanssens <bram.janssens@infosupport.com>
2021-09-08 13:29:53 +0200

    Nog een wijziging.

---
+++ src/main/java/nl/belastingdienst/Kompas.java
@@ -0,0 +1,5 @@
+package nl.belastingdienst;
+
+public enum Kompas { // enumeratie, opsomming van vaste waardes, since java 5 language feature
+    N, O, Z, W
+}
--- src/main/java/nl/belastingdienst/MyFirstClass.java
+++ src/main/java/nl/belastingdienst/MyFirstClass.java
@@ -1,5 +1,7 @@
 package nl.belastingdienst; // package name
 
+// import java.lang.*; // impliciet aanwezig.
+
 public class MyFirstClass { // class name
 
     // Fully qualified class name:
@@ -8,8 +10,16 @@ public class MyFirstClass { // class name
     // commentaar  = single line comment
 
     public static void main(String[] args) { // de main methode
-        System.out.println("String hello world!");
+        String message = "String hello world!";
+        System.out.println(message);
         System.out.println("Nog iets...");
+
+        int i = message.length();
+        String message5 = message.repeat(5);  // API feature
+
+        // Java release
+        // - language features
+        // - API features (add/remove/update)
     }
 
 }
