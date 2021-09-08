af7b45086627637cd9f95a448091d67ecf95b25e
bramjanssens <bram.janssens@infosupport.com>
2021-09-08 11:11:02 +0200

    Nog een sout toegevoegd.

--- src/main/java/nl/belastingdienst/MyFirstClass.java
+++ src/main/java/nl/belastingdienst/MyFirstClass.java
@@ -9,6 +9,7 @@ public class MyFirstClass { // class name
 
     public static void main(String[] args) { // de main methode
         System.out.println("String hello world!");
+        System.out.println("Nog iets...");
     }
 
 }
