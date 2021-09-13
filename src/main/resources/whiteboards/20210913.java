d3163a61b3f3f31f1088a4a9ac9854e9bbc4986e
bramjanssens <bram.janssens@infosupport.com>
2021-09-13 15:44:59 +0200

    whiteboard naar .java gezet.

---
+++ src/main/resources/whiteboards/20210913.java
@@ -0,0 +1,23 @@
+Author marcel=new Author();
+
+Author bea=marcel; // = uitspreken als "wordt"
+
+int i=9;
+
+if(i==10){ // == uitspreken als "is (gelijk aan)"
+
+}
+
+// Methode declareren:
+
+.A..B..C.(.D.) {
+    // return ...; // is van type B
+    // geen return = void
+}
+
+/*
+    A: public/protected/- ("package private")/private // van meeste naar minste zichtbaarheid
+    B: return type van deze methode
+    C: kies een leuke naam (camel case, zonder spaties, is een identifier, regels (bijv. niet met getal beginnen)
+    D: OPTIONEEL! een komma gescheiden lijst van parameters; één param is: <type> <identifier> (i.e. variabele), bijv.: String naam, int age
+*/
