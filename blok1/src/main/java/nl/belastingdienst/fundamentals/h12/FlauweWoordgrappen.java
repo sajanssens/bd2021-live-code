ce5ea59ff13e4c4a952972a03aae5d9237da69cd
bramjanssens <bram.janssens@infosupport.com>
2021-10-04 14:40:34 +0200

    Method generified.

--- blok1/src/main/java/nl/belastingdienst/fundamentals/h12/FlauweWoordgrappen.java
+++ blok1/src/main/java/nl/belastingdienst/fundamentals/h12/FlauweWoordgrappen.java
@@ -4,18 +4,27 @@ import java.lang.reflect.Field;
 
 public class FlauweWoordgrappen {
 
-    public void process(Class<Person> c, Person p) {
+    // member dependent of T
+    public <T> void process(Class<T> c, T anObjectOfTypeT) {
         for (Field field : c.getDeclaredFields()) {
             if (field.isAnnotationPresent(Bram.class)) {
                 try {
                     Bram annotation = field.getAnnotation(Bram.class);
                     field.setAccessible(true);
-                    field.set(p, "Waarom konden de Beatles niet klaverjassen? " + annotation.value() + " :-)!");
+                    field.set(anObjectOfTypeT, "Waarom konden de Beatles niet klaverjassen? " + annotation.value() + getTrailingString(annotation.level()));
                 } catch (IllegalAccessException e) {
                     System.err.println("Jammer joh!");
                 }
             }
         }
     }
+
+    // members not dependent of T
+    // ...
+    private String getTrailingString(int level) {
+        String exclamation = "!".repeat(level);
+        return " :-)" + exclamation;
+    }
+    // ...
 
 }
