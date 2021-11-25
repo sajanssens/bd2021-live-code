91b2874c1cbfc079f77c1faced8f59e2a4166423
bramjanssens <bram.janssens@infosupport.com>
2021-11-25 11:20:03 +0100

    BadRequest added.

--- blok4/rest/src/main/java/nl/belastingdienst/rest/resources/ContactsResource.java
+++ blok4/rest/src/main/java/nl/belastingdienst/rest/resources/ContactsResource.java
@@ -8,9 +8,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import java.util.List;
-import java.util.Optional;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static nl.belastingdienst.rest.util.Responses.badRequest;
 
 @Path("contacts")
 public class ContactsResource {
@@ -19,15 +19,14 @@ public class ContactsResource {
 
     @GET
     @Produces(APPLICATION_JSON)
-    public List<Contact> getAll_MaarJeMagDitZelfWeten() {
+    public List<Contact> getAll() {
         return contactDao.getContacts();
     }
 
     @GET @Path("{id}")
     @Produces(APPLICATION_JSON)
-    public Contact get(@PathParam("id") long id){
-        Optional<Contact> contact = contactDao.getContact(id);
-        return contact.orElse(null);
+    public Contact get(@PathParam("id") long id) {
+        return contactDao.getContact(id).orElseThrow(() -> badRequest(id));
     }
 
     // ....???
